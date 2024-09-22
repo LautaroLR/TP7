c489493bd05d1b1a40df7b2f3303377d5aabbfb3
Lautaro <llr.lomas@gmail.com>
2024-09-22 20:21:29 -0300

    Se añade ejercicio 1 tp 7

---
+++ src/ej1/Enfermedad.java
@@ -0,0 +1,13 @@
+package ej1;
+
+public class Enfermedad {
+//CONSIGNA
+/*Una enfermedad tiene un nombre asociado y un conjunto de estados patológicos
+ *que deben ser tratados. Por ejemplo: Cochinilla (estados patológicos: 
+ *deshidratación, hojas amarillas). Aclaración: los estados patológicos 
+ *de una enfermedad se corresponden con los estados patológicos que los 
+ *productos son capaces de tratar.*/
+//ATRIBUTOS DE INSTANCIA
+	private String nombre;
+	private ArrayList<String> patologíasQueTrata;
+}
---
+++ src/ej1/ProductoQuimico.java
@@ -0,0 +1,67 @@
+package ej1;
+
+import java.util.ArrayList;
+
+public class ProductoQuimico {
+//CONSIGNA
+	/*
+	 * Un producto químico tiene asociado un nombre, un conjunto cultivos donde se
+	 * desaconseja su uso, y un conjunto de estados patológicos que pueden
+	 * observarse sobre los cultivos, y que es capaz de tratar.
+	 */
+//ATRIBUTOS
+	private String nombre;
+	private ArrayList<Cultivo> noRecomendados;
+	private ArrayList<String> estadosPatologicos;
+
+//CONSTRUCTOR	
+	public ProductoQuimico(String nombre) {
+		super();
+		this.nombre = nombre;
+		this.noRecomendados = new ArrayList<Cultivo>();
+		this.estadosPatologicos = new ArrayList<String>();
+	}
+
+//GETTERS Y SETTERS
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+//MÉTODOS DE INSTANCIA
+	
+	public void addNoRecomendado(Cultivo cc) {
+		if (!noRecomendados.contains(cc))
+			noRecomendados.add(cc);
+	}
+
+	public void addEstadoPatologico(String ss) {
+		if (!estadosPatologicos.contains(ss))
+			estadosPatologicos.add(ss);
+	}
+	
+	public boolean equals(Object obj) {
+		try {
+			ProductoQuimico otro = (ProductoQuimico)obj;
+			if (this.nombre==otro.getNombre()) {
+				return true;
+			}
+		} catch (Exception e) {
+			return false;
+		}
+		return false;
+	}
+	
+	public boolean trataEstadoPatologico (String sintoma) {
+		return this.estadosPatologicos.contains(sintoma);
+	}
+	
+	public String toString() {
+		return "Producto Químico [nombre=" + nombre + ", No recomendados=" + noRecomendados + "]";
+	}
+	   
+
+}
